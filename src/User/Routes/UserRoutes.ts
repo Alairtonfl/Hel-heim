018e902f73a7143bb90d0d5f3d376ef9797cc73d
Alairtonfl <alairtonlima3@gmail.com>
2021-12-14 15:09:20 -0300

    DeleteUser implemented

--- src/User/Routes/UserRoutes.ts
+++ src/User/Routes/UserRoutes.ts
@@ -4,6 +4,7 @@ import FindAllUsersController from '@User/Controllers/FindAllUsersController';
 import CreateUserController from '@User/Controllers/CreateUserController';
 import AuthController from '@User/Controllers/AuthController';
 import UpdateUserController from '@User/Controllers/UpdateUserController';
+import DeleteUserController from '@User/Controllers/DeleteUserController';
 
 const userRoutes = Router();
 
@@ -12,5 +13,6 @@ userRoutes.get('/users', FindAllUsersController.handle);
 userRoutes.get('/user/:id', FindUserByIdController.handle);
 userRoutes.post('/user', CreateUserController.handle);
 userRoutes.put('/user/:id', UpdateUserController.handle);
+userRoutes.delete('/user/:id', DeleteUserController.handle);
 
 export default userRoutes;
