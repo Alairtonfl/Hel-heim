706f4213a9f97acd9ebfd33bceeb0f02999c215b
alairtonfl <alairtonlima3@gmail.com>
2021-12-19 19:56:50 -0300

    FindQuestionsByUserId impemented

--- src/UseCases/Question/Controllers/CreateQuestionController.ts
+++ src/UseCases/Question/Controllers/CreateQuestionController.ts
@@ -3,7 +3,8 @@ import CreateQuestionService from '@Question/Services/CreateQuestionService';
 
 export default new class CreateQuestionController {
   async handle(req: Request, res: Response) {
-    const { question, userId, answers } = req.body;
+    const { question, answers } = req.body;
+    const { userId } = req;
     const result = await CreateQuestionService.execute({
       question, userId, answers,
     });
--- src/UseCases/Question/Routes/QuestionRoutes.ts
+++ src/UseCases/Question/Routes/QuestionRoutes.ts
@@ -1,10 +1,13 @@
 import CreateQuestionController from '@Question/Controllers/CreateQuestionController';
 import FindAllQuestionsController from '@Question/Controllers/FindAllQuestionsController';
+import FindQuestionsByUserIdController from '@Question/Controllers/FindQuestionsByUserIdController';
 import { Router } from 'express';
+import authMiddleware from 'src/Middlewares/AuthMiddleware';
 
 const questionRoutes = Router();
 
-questionRoutes.post('/question', CreateQuestionController.handle);
+questionRoutes.post('/question', authMiddleware, CreateQuestionController.handle);
 questionRoutes.get('/question', FindAllQuestionsController.handle);
+questionRoutes.get('/questionuserid', authMiddleware, FindQuestionsByUserIdController.handle);
 
 export default questionRoutes;
