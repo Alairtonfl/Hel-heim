662dbda7b387a8861a1a548114cb4b0798b5d395
antonio.lima <antonio.lima@fitbank.com.br>
2021-12-20 09:44:59 -0300

    FindUserByName implemented

--- src/UseCases/User/Routes/UserRoutes.ts
+++ src/UseCases/User/Routes/UserRoutes.ts
@@ -5,6 +5,8 @@ import CreateUserController from '@User/Controllers/CreateUserController';
 import AuthController from '@User/Controllers/AuthController';
 import UpdateUserController from '@User/Controllers/UpdateUserController';
 import DeleteUserController from '@User/Controllers/DeleteUserController';
+import authMiddleware from 'src/Middlewares/AuthMiddleware';
+import FindUserByNameController from '@User/Controllers/FindUserByNameController';
 
 const userRoutes = Router();
 
@@ -14,5 +16,6 @@ userRoutes.get('/user/:id', FindUserByIdController.handle);
 userRoutes.post('/user', CreateUserController.handle);
 userRoutes.put('/user/:id', UpdateUserController.handle);
 userRoutes.delete('/user/:id', DeleteUserController.handle);
+userRoutes.get('user/:name', authMiddleware, FindUserByNameController.handle);
 
 export default userRoutes;
